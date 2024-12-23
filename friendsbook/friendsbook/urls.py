614e0398f61e700026a6f796310d6ae8488e726f
= <=>
2024-12-23 16:25:43 +0330

    setup

--- friendsbook/friendsbook/urls.py
+++ friendsbook/friendsbook/urls.py
@@ -1,8 +1,12 @@
-"""
-URL configuration for friendsbook project.
+from post.views import post_detail, post_feed, add_post
+from profilepage.views import profile_detail
+from useraccount.views import useraccount_edit
+from business.views import business_listing
+
+"""friendsbook URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,4 +23,10 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', post_feed, name='home'),
+    path('post/<int:post_id>', post_detail, name='post'),
+    path('add_post', add_post),
+    path('profile/<int:profilepage_id>', profile_detail, name='profile page'),
+    path('useraccount/<int:useraccount_id>/edit', useraccount_edit, name='edit user profile'),
+    path('businesses', business_listing, name='business_listing')
 ]
